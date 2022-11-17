db7ed7778a5b00c325d0dc315a56c26917e0a0d4
Sergey Lunev <lunev.s@gmail.com>
2022-11-17 19:49:37 +0300

    level 1 and level 2 ready

--- src/com/github/tel_ran_de/hw1/Level1.java
+++ src/com/github/tel_ran_de/hw1/Level1.java
@@ -1,2 +1,30 @@
-package com.github.tel_ran_de.hw1;public class Level1 {
+package com.github.tel_ran_de.hw1;
+
+public class Level1 {
+    public static void main(String[] args) {
+        // symbol type
+        char c = 'G';
+
+        // whole numbers
+        byte b = 4;
+        short s = 56;
+        int i = 67;
+        long l = 12121L;
+
+        // floating point numbers
+        float f = 4.7333436F;
+        double d = 4.355453532;
+
+        // logical type
+        boolean bf = true;
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(bf);
+    }
 }
--- src/com/github/tel_ran_de/hw1/Level2.java
+++ src/com/github/tel_ran_de/hw1/Level2.java
@@ -1,2 +1,19 @@
-package com.github.tel_ran_de.hw1;public class Level2 {
+package com.github.tel_ran_de.hw1;
+
+public class Level2 {
+    public static void main(String[] args) {
+        // symbol type
+        int inputNumber = 345;
+
+        // calculate parts of number
+        int units = inputNumber % 10;
+        int dozens = (inputNumber / 10) % 10;
+        int hundreds = inputNumber / 100;
+
+        // output
+        System.out.println("input number: " + inputNumber);
+        System.out.println("units: " + units);
+        System.out.println("dozens: " + dozens);
+        System.out.println("hundreds: " + hundreds);
+    }
 }
