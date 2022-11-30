7bed21aca4e95121831197961ed05b538d690cc3
Sergey Lunev <lunev.s@gmail.com>
2022-11-30 23:58:41 +0300

    lesson 7

--- src/com/github/tel_ran_de/lesson7/Main.java
+++ src/com/github/tel_ran_de/lesson7/Main.java
@@ -1,30 +1,47 @@
 package com.github.tel_ran_de.lesson7;
 
-import java.util.Scanner;
 
 public class Main {
 
+    static final int MAX_ARGUMENT = 999;
+
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        int el1 = sc.nextInt();
-        String op = sc.next();
-        char opC = op.charAt(0);
-        int el2 = sc.nextInt();
-
+        // блок ввода данных и определения переменных
+        InputScaner inp = new InputScaner();
         double res = 0;
+
+        int el1 = inp.getIntValue("Enter 1-st operand");
+        String op = inp.getStringValue("Enter sign");
+        char opC = op.charAt(0);
+        int el2 = inp.getIntValue("Enter 2-nd operand");
+
+
+        // блок вычислительной логики
         if (opC == '+') {
             res = el1 + el2;
         } else if (opC == '/' && el2 != 0) {
             res = (double) el1 / el2;
+        } else if (opC == '-') {
+            res = el1 - el2;
+        } else if (opC == '*') {
+            res = el1 * el2;
         }
 
+        // блок проверки ошибок
         if (opC == '/' && el2 == 0) {
             System.out.println("на 0 делить нельзя");
+        } else if (el1 > MAX_ARGUMENT || el2 > MAX_ARGUMENT || res > MAX_ARGUMENT) {
+            System.out.println("Ошибка: переполнение разрядной сетки");
         } else {
             System.out.println(res);
         }
+
+    }
+}
+
+
 
 //        System.out.println("необходимо реагента: " + ParkingPlace.getReagentNeed() + " на " + ParkingPlace.parkingNum + " парковок");
 //        ParkingPlace pl1 = new ParkingPlace("001");
@@ -49,7 +66,4 @@ public class Main {
 //        System.out.println(pl1);
 //        System.out.println(pl2);
 //        System.out.println(pl3);
-
-    }
-}
 
